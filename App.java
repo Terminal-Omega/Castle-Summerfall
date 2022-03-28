75a8c4617ced3680c8b709152a3a1a906f25d8e0
Thomas Dick <thomas.ed.dick@gmail.com>
2022-03-28 10:00:51 -0600

    exit command for UI is working

--- App.java
+++ App.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 
 public class App {
 
     public static void main(String[] args) {
-
+        UI.helpCommand("all");
+        Scanner input = new Scanner(System.in);
+        String inputCommand;
+        do{
+            System.out.print("> ");
+            inputCommand = input.nextLine();
+        }while(inputCommand.equals(UI.Commands.EXIT.getStrCommand()) == false);
     }
 }
--- UI.java
+++ UI.java
@@ -1,3 +1,36 @@
 public class UI {
+    enum Commands {
+        INSPECT("inspect"),
+        TAKE("take"),
+        LOOK_AROUND("look around"),
+        DROP("drop"),
+        MOVE("move"),
+        ATTACK("attack"),
+        DRINK("drink"),
+        CAST("cast"),
+        HELP("help"),
+        EXIT("exit");
+
+        private String strCommand;
+
+        private Commands(String command) {
+            this.strCommand = command;
+        }
+
+        public String getStrCommand() {
+            return this.strCommand;
+        }
+        }
+
     
+    public static void helpCommand(String command){
+        if (command.equals("all")){
+            System.out.println("\nThis is a text based adventure game where you fight monsters. How many floors can you decesend?\n");
+            System.out.println("Commands:");
+            for (Commands name : Commands.values()){
+                System.out.println("    " + name + ",");
+            }
+            System.out.println("Use help (command name) to learn about that command\n");
+        }
+    }
 }
