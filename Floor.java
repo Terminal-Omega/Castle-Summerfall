13df10162c052c0c860bfc96e772936b05dd7885
Corbanator <corbandioxide42@gmail.com>
2022-03-28 14:45:23 -0600

    Add a floor constructor

--- Floor.java
+++ Floor.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 public class Floor {
     private ArrayList<ArrayList<Room>> rooms; // Stores Coordinates literally based on the location within the vector.
 
+    public Floor(ArrayList<ArrayList<Room>> rooms) {
+        this.rooms = rooms;
+    }
+
     public Room getRoom(int xCoord, int yCoord){
         return rooms.get(xCoord).get(yCoord);
     }
