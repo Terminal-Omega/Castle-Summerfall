b56330a542d17f9c77f7181c8fd43c74f0cf8372
The-Watcher-213 <67605401+The-Watcher-213@users.noreply.github.com>
2022-03-31 14:08:19 -0600

    messing around with moving room and possible weapon stuff

--- Weapon.java
+++ Weapon.java
@@ -3,6 +3,7 @@ public class Weapon extends Interactable {
     public Weapon(int size, int weight, boolean canBePickedUp, String name, String description) {
         super(size, weight, canBePickedUp, name, description);
     }
+
     protected int attackSpeed;
     protected String type;
     protected int damage;
@@ -11,4 +12,10 @@ public class Weapon extends Interactable {
     protected String name;
     protected boolean sharp;
     protected int pierce;
+    // Knife type:CC, damage:3-5, name:knife, pierce:3
+    // Short Sword type:CC, damage:5-8, name:Short Sword, pierce:5
+    // Sword type:CC, damage:6-9, name:Sword, Pierce:6
+    // Great Sword type:CC, damage:10-15, name:Great Sword, Pierce, 10
+    // Rapier type:CC, damage:6-9, name:Rapier, Pierce: 13
+
 }
