42dfc6e1782cd5ab5a20c99b6b21b9ff4d23f42c
Corbanator <corbandioxide42@gmail.com>
2022-03-31 14:28:57 -0600

    Add some basic weapons to the generator; not functional yet.

--- Generator.java
+++ Generator.java
@@ -9,8 +9,37 @@ public class Generator {
         return result;
     }
 
-    public Interactable chest(){
+    //interactables
+    private Interactable chest(){
         Interactable result = new Interactable();
         return result;
     }
+
+
+    //Weapons
+    private Weapon knife(){
+        Weapon result = new Weapon(1, 5, true,"Knife","It's kinda sharp.");
+        return result;
+    }
+
+    private Weapon shortSword(){
+        Weapon result = new Weapon(2, 8, true,"Short Sword","Like a knife, but long.");
+        return result;
+    }
+
+    private Weapon sword(){
+        Weapon result = new Weapon(2, 9, true,"Sword","It might hurt someone");
+        return result;
+    }
+
+    private Weapon greatSword(){
+        Weapon result = new Weapon(3,11,true,"Greatsword","This is a big sword.");
+        return result;
+    }
+
+    private Weapon rapier(){
+        Weapon result = new Weapon(1, 6, true,"Rapier","Thin and deadly.");
+        return result;
+    }
+
 }
