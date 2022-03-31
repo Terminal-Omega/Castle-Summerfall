1421facb05494b00613673b002f62a5f2b8b5395
Corbanator <corbandioxide42@gmail.com>
2022-03-31 14:42:23 -0600

    Generator is now technically functional. There are chests and some weapons with no damage stat.

--- Generator.java
+++ Generator.java
@@ -1,43 +1,76 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 //this is a grouping of methods to randomly generate any given game object.
 //A room, an interactable, a floor, an enemy, etc.
 public class Generator {
     //TODO: how do I generate a random interactable?
     public static Interactable generateInteractable(){
-        Interactable result = new Interactable();
+        Random rand = new Random();
+        Interactable result;
+        switch (rand.nextInt(1)) {
+            case 0:
+                result = chest();
+                break;
+            case 1:
+                result = knife();
+                break;
+            case 2:
+                result = shortSword();
+                break;
+            case 3:
+                result = sword();
+                break;
+            case 4:
+                result = greatSword();
+                break;
+            case 5:
+                result = rapier();
+                break;
+            default:
+                result = null;
+                break;
+        }
 
 
         return result;
     }
 
     //interactables
-    private Interactable chest(){
-        Interactable result = new Interactable();
+    private static Interactable chest(){
+        Random rand = new Random();
+        ArrayList<Interactable> chestInventory = new ArrayList<Interactable>();
+
+        for(int i = 0;i<rand.nextInt(3);i++){
+            chestInventory.add(generateInteractable());
+        }
+
+        Interactable result = new Interactable(5,20,false,"Chest","It's a box.",chestInventory);
         return result;
     }
 
-
     //Weapons
-    private Weapon knife(){
+    private static Weapon knife(){
         Weapon result = new Weapon(1, 5, true,"Knife","It's kinda sharp.");
         return result;
     }
 
-    private Weapon shortSword(){
+    private static Weapon shortSword(){
         Weapon result = new Weapon(2, 8, true,"Short Sword","Like a knife, but long.");
         return result;
     }
 
-    private Weapon sword(){
+    private static Weapon sword(){
         Weapon result = new Weapon(2, 9, true,"Sword","It might hurt someone");
         return result;
     }
 
-    private Weapon greatSword(){
+    private static Weapon greatSword(){
         Weapon result = new Weapon(3,11,true,"Greatsword","This is a big sword.");
         return result;
     }
 
-    private Weapon rapier(){
+    private static Weapon rapier(){
         Weapon result = new Weapon(1, 6, true,"Rapier","Thin and deadly.");
         return result;
     }
--- Interactable.java
+++ Interactable.java
@@ -8,12 +8,17 @@ public class Interactable {
     private String name;
     private String description;
 
-    public Interactable(int size, int weight, boolean canBePickedUp, String name, String description) {
+    public Interactable(int size, int weight, boolean canBePickedUp, String name, String description, ArrayList<Interactable> inventory) {
         this.size = size;
         this.weight = weight;
         this.canBePickedUp = canBePickedUp;
         this.name = name;
         this.description = description;
+        this.inventory = inventory;
+    }
+
+    public Interactable(int size, int weight, boolean canBePickedUp, String name, String description){
+        this(size, weight, canBePickedUp,name,description,null);
     }
     public Interactable(){};
 
