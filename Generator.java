f1d4ef49c07a78a724f70fae57a91db9386c85ba
The-Watcher-213 <67605401+The-Watcher-213@users.noreply.github.com>
2022-04-01 08:40:33 -0600

    fixing the weapons with how I had acidently broken them by messing around with weapon

--- Generator.java
+++ Generator.java
@@ -5,33 +5,33 @@ import java.util.Random;
 //A room, an interactable, a floor, an enemy, etc.
 public class Generator {
 
-    public static Floor generateFloor(int xSize, int ySize){
+    public static Floor generateFloor(int xSize, int ySize) {
         ArrayList<ArrayList<Room>> rooms = new ArrayList<>();
-        for(int i = 0;i<xSize;i++){
+        for (int i = 0; i < xSize; i++) {
             ArrayList<Room> column = new ArrayList<>();
-            for(int j = 0;j<ySize;j++){
-                column.add(generateRoom(1,3));
+            for (int j = 0; j < ySize; j++) {
+                column.add(generateRoom(1, 3));
             }
             rooms.add(column);
         }
         Floor result = new Floor(rooms);
         return result;
     }
-    
-    public static Room generateRoom(int interactableMin, int interactableMax){
+
+    public static Room generateRoom(int interactableMin, int interactableMax) {
         int range = interactableMax - interactableMin;
         Random rand = new Random();
         int loopCount = rand.nextInt(range) + interactableMin;
         ArrayList<Interactable> roomInventory = new ArrayList<Interactable>();
-        for(int i = 0; i<loopCount;i++){
+        for (int i = 0; i < loopCount; i++) {
             roomInventory.add(generateInteractable());
         }
-        Room result = new Room(roomInventory,"This is a room");
-        return result; 
+        Room result = new Room(roomInventory, "This is a room");
+        return result;
     }
 
-    //below this is all the interactable generation.
-    public static Interactable generateInteractable(){
+    // below this is all the interactable generation.
+    public static Interactable generateInteractable() {
         Random rand = new Random();
         Interactable result;
         switch (rand.nextInt(1)) {
@@ -58,46 +58,45 @@ public class Generator {
                 break;
         }
 
-
         return result;
     }
 
-    //interactables
-    private static Interactable chest(){
+    // interactables
+    private static Interactable chest() {
         Random rand = new Random();
         ArrayList<Interactable> chestInventory = new ArrayList<Interactable>();
         int loopCount = rand.nextInt(3);
-        for(int i = 0;i<loopCount;i++){
+        for (int i = 0; i < loopCount; i++) {
             chestInventory.add(generateInteractable());
         }
 
-        Interactable result = new Interactable(5,20,false,"Chest","It's a box.",chestInventory);
+        Interactable result = new Interactable(5, 20, false, "Chest", "It's a box.", chestInventory);
         return result;
     }
 
-    //Weapons
-    private static Weapon knife(){
-        Weapon result = new Weapon(1, 5, true,"Knife","It's kinda sharp.");
+    // Weapons
+    private static Weapon knife() {
+        Weapon result = new Weapon(1, 5, true, "Knife", "It's kinda sharp.", 3);
         return result;
     }
 
-    private static Weapon shortSword(){
-        Weapon result = new Weapon(2, 8, true,"Short Sword","Like a knife, but long.");
+    private static Weapon shortSword() {
+        Weapon result = new Weapon(2, 8, true, "Short Sword", "Like a knife, but long.", 5);
         return result;
     }
 
-    private static Weapon sword(){
-        Weapon result = new Weapon(2, 9, true,"Sword","It might hurt someone");
+    private static Weapon sword() {
+        Weapon result = new Weapon(2, 9, true, "Sword", "It might hurt someone", 6);
         return result;
     }
 
-    private static Weapon greatSword(){
-        Weapon result = new Weapon(3,11,true,"Greatsword","This is a big sword.");
+    private static Weapon greatSword() {
+        Weapon result = new Weapon(3, 11, true, "Greatsword", "This is a big sword.", 10);
         return result;
     }
 
-    private static Weapon rapier(){
-        Weapon result = new Weapon(1, 6, true,"Rapier","Thin and deadly.");
+    private static Weapon rapier() {
+        Weapon result = new Weapon(1, 6, true, "Rapier", "Thin and deadly.", 13);
         return result;
     }
 
