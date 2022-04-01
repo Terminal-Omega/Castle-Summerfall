0c3285653f24ae2a51b71bd7afae4c13043b54bd
The-Watcher-213 <67605401+The-Watcher-213@users.noreply.github.com>
2022-04-01 11:13:57 -0600

    Here you go corbain

--- Enemy.java
+++ Enemy.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Enemy extends NPC {
 
@@ -8,8 +9,12 @@ public class Enemy extends NPC {
 
     public Enemy(int xCoord, int yCoord, int health, int AC, int speed, int mana, int strength, int dexterity,
             int constitution, int intelligence, int wisdom, int charisma, int noise, int carryWeight, String name,
-            int shield, String npcAllience) {
+            int shield, String npcAllience, String description) {
         super(xCoord, yCoord, health, AC, speed, mana, strength, dexterity, constitution, intelligence, wisdom,
-                charisma, noise, carryWeight, shield, name, npcAllience);
+                charisma, noise, carryWeight, shield, name, npcAllience, description);
+    }
+
+    public void noneCombatAI() {
+
     }
 }
--- NPC.java
+++ NPC.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 
 public class NPC extends Actor {
     protected String npcAllience;
+    protected String description;
 
     public NPC() {
 
@@ -10,7 +11,7 @@ public class NPC extends Actor {
 
     public NPC(int xCoord, int yCoord, int health, int AC, int speed, int mana, int strength, int dexterity,
             int constitution, int intelligence, int wisdom, int charisma,
-            int noise, int carryWeight, int shield, String name, String npcAllience) {
+            int noise, int carryWeight, int shield, String name, String npcAllience, String description) {
         super(xCoord, yCoord, health, AC, speed, mana, strength, dexterity, constitution, intelligence, wisdom,
                 charisma, noise,
                 carryWeight, shield, name);
