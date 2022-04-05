188a2f2b6ff089f7849e7566372fca45bac726cd
The-Watcher-213 <67605401+The-Watcher-213@users.noreply.github.com>
2022-04-05 14:42:28 -0600

    better CC method

--- Actor.java
+++ Actor.java
@@ -207,131 +207,27 @@ public class Actor {
         inventory.add(interactable);
     }
 
-    public void takeDamagePlayer(int damage, Player player) {
-        int finalDamage = damage - player.AC;
+    public void takeDamage(int damage) {
+        int finalDamage = damage - AC;
         if (finalDamage < 0) {
-            System.out.print("Your armour protected you. you take 0 damage");
             finalDamage = 0;
         }
-        player.health -= finalDamage;
-        if (player.health <= 0) {
-            System.out.println("You have died");
+        health -= finalDamage;
+        if (health <= 0) {
+            System.out.print("Dead");
         }
-
     }
 
-    public void takeDamageNPC(int damage, NPC target) {
-        int finalDamage = damage - target.AC;
-        if (finalDamage < 0) {
-            System.out.print("The Attack failed to harm the target");
-            finalDamage = 0;
-        }
-        target.health -= finalDamage;
-        if (target.health <= 0) {
-            System.out.println("A NPC has been killed");
-        }
-
-    }
-
-    public void heal(int heal, String name) {
+    public void heal(int heal) {
         health += heal;
-        if (name == "player") {
-            System.out.printf("you just healed %d amount of health new health is", heal, health);
-        }
+
     }
 
-    public void moveNorth(boolean doorLocked, boolean doorBarred) {
-        if (doorLocked == false && doorBarred == false) {
-            yCoord++;
-        } else if (doorLocked == true) {
-            System.out.print("Door is locked");
+    public void closeCombat(Weapon weapon, Actor target) {
+        if (rand.nextInt(100) + 1 <= weaponSkill) {
+            target.takeDamage(weapon.damage);
         } else {
-            System.out.print("The Door is barred");
-        }
-
-    }
-
-    public void moveSouth() {
-        yCoord--;
-    }
-
-    public void moveWest() {
-        xCoord--;
-    }
-
-    public void moveEast() {
-        xCoord++;
-    }
-
-    public ArrayList<Interactable> search() {
-        return inventory; // To be replaced when we have the variable for the items in the room
-    }
-
-    /*
-     * this works a little werid basicly you WANT negativeModdifier because inorder
-     * to hit you need to roll below you BS so negativeModdifiers make it easier to
-     * hit possitve modifiers
-     * make it harder to hit. If anyone reading this wants a better explination
-     * speak to Xander in person because it is easier to describe that way.
-     */
-    /*
-     * public void magic(int minDamage, int maxDamage, int targetHealth, String
-     * targetName, int BS,
-     * int negativeModdifier,
-     * int possitveModdifier, int targetAC) {
-     * int finalDamage = rand.nextInt(minDamage, maxDamage);
-     * 
-     * if (rand.nextInt(100) + 1 - negativeModdifier + possitveModdifier <= BS) {
-     * takeDamagePlayer(finalDamage, targetName, targetAC);
-     * } else {
-     * System.out.println("Miss");
-     * }
-     * 
-     * }
-     */
-    /*
-     * public void ranged(int arrowCount, int damageMin, int damageMax, int
-     * targetHealth, String targetName, int BS,
-     * int negativeModdifier,
-     * int possitiveModdifier, int targetAC) {
-     * int finalDamage = rand.nextInt(damageMin, damageMax);
-     * if (arrowCount > 0) {
-     * if (rand.nextInt(100) + 1 - negativeModdifier + possitiveModdifier <= BS) {
-     * takeDamagePlayer(finalDamage, targetName, targetAC);
-     * } else {
-     * System.out.println("Miss");
-     * }
-     * }
-     * }
-     */
-    /*
-     * public void closeCombat(int damageMin, int damageMax, int targetHealth,
-     * String targetName, int WS,
-     * int negativeModdifier, int possitiveModdifier, int targetAC) {
-     * int finalDamage = rand.nextInt(damageMin, damageMax);
-     * if (rand.nextInt(100) + 1 - negativeModdifier + possitiveModdifier <= WS) {
-     * takeDamage(finalDamage, targetName, targetAC);
-     * } else {
-     * System.out.println("Miss");
-     * }
-     * }
-     */
-
-    public void closeCombatPlayer(Player player, Weapon weapon, NPC npcTarget) {
-        if (rand.nextInt(100) + 1 <= player.getWeaponSkill()) {
-            takeDamagePlayer(weapon.damage, player);
-        } else {
-            System.out.println("Miss");
-        }
-    }
-
-    public void closeCombatNPC(NPC npc, Weapon weapon, String target, Player player, NPC npcTarget) {
-        if (rand.nextInt(100) + 1 <= npc.getWeaponSkill()) {
-            if (target == "Player") {
-                takeDamagePlayer(weapon.damage, player);
-            } else {
-                takeDamageNPC(weapon.damage, npcTarget);
-            }
+            System.out.print("miss");
         }
     }
 
@@ -344,14 +240,6 @@ public class Actor {
 
             }
         }
-    }
-
-    public void openInventory() {
-
-    }
-
-    public void grabFromInventory() {
-
     }
 
 }
