2c07b917d38f5e61eea4fab397d780b13d2198e4
Corbanator <corbandioxide42@gmail.com>
2022-04-05 15:05:25 -0600

    Made method static

--- effect.java
+++ effect.java
@@ -3,12 +3,12 @@ import java.util.Random;
 import java.util.regex.Matcher;
 import java.lang.Integer;
 public class effect {
-    public void applyEffect(String effect, Actor target){
+    public static void applyEffect(String effect, Actor target){
         Random rand = new Random();
 
         Pattern damage = Pattern.compile("damage ([0-9]*(-([0-9]*))?)");
 
-        
+
         Matcher damageMatcher = damage.matcher(effect);
         if(damageMatcher.find()){
             if(damageMatcher.group(3)!=""){
