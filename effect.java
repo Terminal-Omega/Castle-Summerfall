b67f7a6d71eaec71e5e4909832155eb5e079f8b8
Corbanator <corbandioxide42@gmail.com>
2022-04-05 15:05:09 -0600

    Began work on the Effect Class

--- effect.java
+++ effect.java
@@ -1,3 +1,21 @@
+import java.util.regex.Pattern;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.lang.Integer;
 public class effect {
+    public void applyEffect(String effect, Actor target){
+        Random rand = new Random();
 
+        Pattern damage = Pattern.compile("damage ([0-9]*(-([0-9]*))?)");
+
+        
+        Matcher damageMatcher = damage.matcher(effect);
+        if(damageMatcher.find()){
+            if(damageMatcher.group(3)!=""){
+                int damageAmount = rand.nextInt(Integer.parseInt(damageMatcher.group(1)));
+            }
+        }
+
+
+    }
 }
