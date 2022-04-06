bfc3b5f62d0d76ec56bf8effd3a6af4fec9cb3cd
The-Watcher-213 <67605401+The-Watcher-213@users.noreply.github.com>
2022-04-06 09:29:21 -0600

    possible variables for NPC to figure out player direction

--- NPC.java
+++ NPC.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 public class NPC extends Actor {
     protected String npcAllience;
     protected String description;
+    private Random rand = new Random();
+    final int floorsize = 5;
+    protected boolean playerDetected;
+    protected boolean playerSeen;
+    protected boolean playerHeard;
+    protected String playerDirection;
 
     public NPC() {
 
@@ -26,35 +32,42 @@ public class NPC extends Actor {
         npcAllience = allience;
     }
 
-    public void npcTurnCheck(Player player) {
-        if (player.getXCoord() == xCoord && player.getYCoord() == yCoord) {
-            npcTurnNoneCombat();
-        } else {
-            npcTurnCombat();
-        }
-    }
-
-    public void npcTurnCombat() {
+    public void npcTurnAllience(Player player, int floorSize) {
         if (npcAllience == "Enemy") {
-            enemyTurnCombat();
+            if (player.getXCoord() == xCoord && player.getYCoord() == yCoord) {
+                enemyTurnCombat();
+            } else {
+                enemyTurnNoneCombat(floorSize);
+            }
         } else if (npcAllience == "Friendly") {
-            friendlyTurnCombat();
+
         } else {
-            neutralTurnCombat();
+
         }
     }
 
-    public void npcTurnNoneCombat() {
-        if (npcAllience == "Enemy") {
-            enemyTurnNoneCombat();
-        } else if (npcAllience == "Friendly") {
-            friendlyTurnNoneCombat();
-        } else {
-            neutralTurnNoneCombat();
-        }
-    }
+    public void enemyTurnNoneCombat(int floorSize) {
+        int direction = rand.nextInt();
+        switch (direction) {
+            case 0:
+                if (yCoord < floorSize - 1 && yCoord >= 0) {
+                    yCoord++;
+                }
+            case 1:
+                if (yCoord < floorSize - 1 && yCoord > 0) {
+                    yCoord--;
+                }
+            case 2:
+                if (xCoord < floorSize - 1 && xCoord >= 0) {
+                    xCoord++;
+                }
+            case 3:
+                if (yCoord < floorSize - 1 && yCoord > 0) {
+                    yCoord--;
+                }
+            default:
 
-    public void enemyTurnNoneCombat() {
+        }
 
     }
 
