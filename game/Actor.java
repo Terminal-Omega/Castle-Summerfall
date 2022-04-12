4dd20ee28e45c2f74515709c5c2d40e364eaa7b1
The-Watcher-213 <67605401+The-Watcher-213@users.noreply.github.com>
2022-04-12 11:59:15 -0600

    quick fix thing

--- game/Actor.java
+++ game/Actor.java
@@ -505,11 +505,14 @@ public class Actor {
      * @return boolean
      */
     public boolean closeCombat(Weapon weapon, Actor target) {
-        if (rand.nextInt(10) + 1 <= weaponSkill) {
-
+        if (rand.nextInt(100) + 1 <= weaponSkill) {
+            System.out.printf("hit %s%n", target.name);
             return target.takeDamage(weapon.damage, weapon.pierce);
+        } else {
+            System.out.printf("Miss %s%n", target.name);
+            return false;
         }
-        return false;
+
     }
 
     /**
--- game/Updates.java
+++ game/Updates.java
@@ -1,86 +1,103 @@
 package game;
+
 import java.util.regex.Pattern;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.lang.Integer;
+
 public class Updates {
-    
-    /** 
+
+    /**
      * @param effect
      * @param target
      */
-    public static void applyEffect(String effect, Actor target){
+    public static void applyEffect(String effect, Actor target) {
         Random rand = new Random();
 
         Pattern damage = Pattern.compile("damage\\(([0-9]*(-([0-9]*))?)\\)");
         Pattern heal = Pattern.compile("heal\\(([0-9]*(-([0-9]*))?)\\)");
+        Pattern damageNoAC = Pattern.compile("damageNoAc\\(([0-9]*(-([0-9]*))?)\\)");
+        Pattern statChanger = Pattern.compile("stat");
 
         Matcher damageMatcher = damage.matcher(effect);
-        if(damageMatcher.find()){
+        if (damageMatcher.find()) {
             int damageAmount;
-            if(damageMatcher.group(3)!=""){
-                damageAmount = rand.nextInt(Integer.parseInt(damageMatcher.group(3))) + Integer.parseInt(damageMatcher.group(1));
-                // TODO: @Corbanator make this work once Xander pushes  the thing
-            } else{
+            if (damageMatcher.group(3) != "") {
+                damageAmount = rand.nextInt(Integer.parseInt(damageMatcher.group(3)))
+                        + Integer.parseInt(damageMatcher.group(1));
+                // TODO: @Corbanator make this work once Xander pushes the thing
+            } else {
                 damageAmount = Integer.parseInt(damageMatcher.group(1));
             }
+            target.takeDamage(damageAmount, 0);
         }
 
         Matcher healMatcher = heal.matcher(effect);
-        if(damageMatcher.find()){
+        if (damageMatcher.find()) {
             int healAmount;
-            if(damageMatcher.group(3)!=""){
-                healAmount = rand.nextInt(Integer.parseInt(damageMatcher.group(3))) + Integer.parseInt(damageMatcher.group(1));
-                // TODO: @Corbanator make this work once Xander pushes  the thing
-            } else{
+            if (healMatcher.group(3) != "") {
+                healAmount = rand.nextInt(Integer.parseInt(damageMatcher.group(3)))
+                        + Integer.parseInt(damageMatcher.group(1));
+                // TODO: @Corbanator make this work once Xander pushes the thing
+            } else {
                 healAmount = Integer.parseInt(damageMatcher.group(1));
             }
             target.heal(healAmount);
         }
 
+        Matcher damageNoACMathcer = damageNoAC.matcher(effect);
+        if (damageNoACMathcer.find()) {
+            int damageAmount;
+            if (damageNoACMathcer.group(3) != "") {
+                damageAmount = rand.nextInt(Integer.parseInt(damageMatcher.group(3)))
+                        + Integer.parseInt(damageMatcher.group(1));
+
+            } else {
+                damageAmount = Integer.parseInt(damageNoACMathcer.group(1));
+            }
+            target.damageNoAC(damageAmount);
+        }
 
     }
 
-    
-    /** 
+    /**
      * @param actor
      */
-    public static void doPassives(Actor actor){
+    public static void doPassives(Actor actor) {
 
     }
 
-    
-    /** 
+    /**
      * @param player
      * @param floor
      */
-    public static void update(Player player, Floor floor){
+    public static void update(Player player, Floor floor) {
         doPassives(player);
         // TODO: @Corbanator implement noise and LOS
         int size = floor.getNPCs().size();
-        for(int i = 0; i<size;i++){
+        for (int i = 0; i < size; i++) {
             NPC npc = floor.getNPCs().get(i);
             doPassives(npc);
-            if(npc.getHealth() <= 0){
+            if (npc.getHealth() <= 0) {
                 floor.getNPCs().remove(i);
-                System.out.println(npc.getName() +" was killed.");
+                System.out.println(npc.getName() + " was killed.");
             }
             npc.npcTurnAllience(player, floor.getXSize());
             // TODO: @Corbanator notify player if NPC enters the room
         }
     }
 
-    
-    /** 
+    /**
      * @param floor
      */
-    public static void actionUpdate(Floor floor){
-        for(int i = 0; i<floor.getNPCs().size();i++){
+    public static void actionUpdate(Floor floor) {
+        for (int i = 0; i < floor.getNPCs().size(); i++) {
             NPC npc = floor.getNPCs().get(i);
-            if(npc.getHealth() <= 0){
+            if (npc.getHealth() <= 0) {
                 floor.getNPCs().remove(i);
-                System.out.println(npc.getName() +" was killed.");
+                System.out.println(npc.getName() + " was killed.");
             }
         }
     }
+
 }
