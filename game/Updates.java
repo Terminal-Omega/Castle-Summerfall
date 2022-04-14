be952f1362e0d429ad4c88107c1105e2c29e61c2
The-Watcher-213 <67605401+The-Watcher-213@users.noreply.github.com>
2022-04-14 14:31:51 -0600

    This should work.... hopefully

--- game/Updates.java
+++ game/Updates.java
@@ -14,10 +14,10 @@ public class Updates {
     public static void applyEffect(String effect, Actor target) {
         Random rand = new Random();
 
-        Pattern damage = Pattern.compile("damage\\(([0-9]*(-([0-9]*))?)\\)");
-        Pattern heal = Pattern.compile("heal\\(([0-9]*(-([0-9]*))?)\\)");
+        Pattern damage = Pattern.compile("damage\\(([0-9])*(-([0-9]*))?\\)");
+        Pattern heal = Pattern.compile("heal\\(([0-9])*(-([0-9]*))?\\)");
         Pattern damageNoAC = Pattern.compile("damageNoAC\\(([0-9]*(-([0-9]*))?)\\)");
-        Pattern statChanger = Pattern.compile("stat");
+        Pattern statChanger = Pattern.compile("(?<=stat:)(.*):\\s*([0-9]*)(?:-)?([0-9]+)?;");
 
         Matcher damageMatcher = damage.matcher(effect);
         if (damageMatcher.find()) {
@@ -45,17 +45,86 @@ public class Updates {
             target.heal(healAmount);
         }
 
-        Matcher damageNoACMathcer = damageNoAC.matcher(effect);
-        if (damageNoACMathcer.find()) {
+        Matcher damageNoACMatcher = damageNoAC.matcher(effect);
+        if (damageNoACMatcher.find()) {
             int damageAmount;
-            if (damageNoACMathcer.group(3) != "") {
-                damageAmount = rand.nextInt(Integer.parseInt(damageMatcher.group(3)))
-                        + Integer.parseInt(damageMatcher.group(1));
+            if (damageNoACMatcher.group(3) != "") {
+                damageAmount = rand.nextInt(Integer.parseInt(damageNoACMatcher.group(3)))
+                        + Integer.parseInt(damageNoACMatcher.group(1));
 
             } else {
-                damageAmount = Integer.parseInt(damageNoACMathcer.group(1));
+                damageAmount = Integer.parseInt(damageNoACMatcher.group(1));
             }
             target.damageNoAC(damageAmount);
+        }
+
+        Matcher statEffect = statChanger.matcher(effect);
+        if (statEffect.find()) {
+            int effectAmount;
+            int rangeMin;
+            int rangeMax;
+            if (statEffect.group(1).equals("Strength")) {
+                if (statEffect.group(3) != "") {
+                    rangeMin = Integer.parseInt(statEffect.group(2));
+                    rangeMax = Integer.parseInt(statEffect.group(3));
+                    effectAmount = rand.nextInt(rangeMin, rangeMax) + 1;
+
+                } else {
+                    effectAmount = Integer.parseInt(statEffect.group(2));
+                }
+                target.modifiyStrength(effectAmount);
+            } else if (statEffect.group(2).equals("Dexterity")) {
+                if (statEffect.group(3) != "") {
+                    rangeMin = Integer.parseInt(statEffect.group(2));
+                    rangeMax = Integer.parseInt(statEffect.group(3));
+                    effectAmount = rand.nextInt(rangeMin, rangeMax) + 1;
+
+                } else {
+                    effectAmount = Integer.parseInt(statEffect.group(2));
+                }
+                target.modifyDexterity(effectAmount);
+            } else if (statEffect.group(1).equals("Constitution")) {
+                if (statEffect.group(3) != "") {
+                    rangeMin = Integer.parseInt(statEffect.group(2));
+                    rangeMax = Integer.parseInt(statEffect.group(3));
+                    effectAmount = rand.nextInt(rangeMin, rangeMax) + 1;
+
+                } else {
+                    effectAmount = Integer.parseInt(statEffect.group(2));
+                }
+                target.modifyConstitution(effectAmount);
+            } else if (statEffect.group(1).equals("Wisdom")) {
+                if (statEffect.group(3) != "") {
+                    rangeMin = Integer.parseInt(statEffect.group(2));
+                    rangeMax = Integer.parseInt(statEffect.group(3));
+                    effectAmount = rand.nextInt(rangeMin, rangeMax) + 1;
+
+                } else {
+                    effectAmount = Integer.parseInt(statEffect.group(2));
+                }
+                target.modifyWisdom(effectAmount);
+            } else if (statEffect.group(1).equals("Charisma")) {
+                if (statEffect.group(3) != "") {
+                    rangeMin = Integer.parseInt(statEffect.group(2));
+                    rangeMax = Integer.parseInt(statEffect.group(3));
+                    effectAmount = rand.nextInt(rangeMin, rangeMax) + 1;
+
+                } else {
+                    effectAmount = Integer.parseInt(statEffect.group(2));
+                }
+                target.modifyCharisma(effectAmount);
+            } else if (statEffect.group(1).equals("Intelligence")) {
+                if (statEffect.group(3) != "") {
+                    rangeMin = Integer.parseInt(statEffect.group(2));
+                    rangeMax = Integer.parseInt(statEffect.group(3));
+                    effectAmount = rand.nextInt(rangeMin, rangeMax) + 1;
+
+                } else {
+                    effectAmount = Integer.parseInt(statEffect.group(2));
+                }
+                target.modifyIntellignce(effectAmount);
+
+            }
         }
 
     }
